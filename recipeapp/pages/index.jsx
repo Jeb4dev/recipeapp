7222c03eb03a9961ad490935c4ef8bcf74c9d776
Jeb <commit@jeb4.dev>
2024-02-20 17:50:54 +0200

    removed dummy data and sort

--- recipeapp/pages/index.jsx
+++ recipeapp/pages/index.jsx
@@ -3,58 +3,28 @@ import Layout from '../components/Layout';
 import RecipeCard from '../components/RecipeCard';
 import { request } from '../lib/datocms';
 
-const dummyData = [
-  {
-    image: 'https://via.placeholder.com/150',
-    title: 'Recipe 1',
-    description: 'This is a description for Recipe 1.',
-    rating: 4.5,
-  },
-  {
-    image: 'https://via.placeholder.com/150',
-    title: 'Recipe 2',
-    description: 'This is a description for Recipe 2.',
-    rating: 4.0,
-  },
-  {
-    image: 'https://via.placeholder.com/150',
-    title: 'Recipe 3',
-    description: 'This is a description for Recipe 3.',
-    rating: 5.0,
-  },
-  {
-    image: 'https://via.placeholder.com/150',
-    title: 'Recipe 4',
-    description: 'This is a description for Recipe 4.',
-    rating: 3.5,
-  },
-  {
-    image: 'https://via.placeholder.com/150',
-    title: 'Recipe 5',
-    description: 'This is a description for Recipe 5.',
-    rating: 4.5,
-  },
-];
 const HomePage = (props) => {
   const recipes = props.data.allRecipes;
   return (
     <Layout>
       <div>
-        <h1 className="text-2xl font-bold mb-4">Tykätyimmät reseptit</h1>
-        <div className="flex flex-wrap justify-around">
-          {recipes
-            .sort((a, b) => b.likes - a.likes)
-            .map((recipe, index) => (
-              <RecipeCard key={index} recipe={recipe} />
-            ))}
+        <div className={"max-w-7xl mx-auto"}>
+          <h1 className="text-2xl font-bold mb-4">Tykätyimmät reseptit</h1>
+          <div className="flex flex-wrap justify-between">
+            {recipes
+              .sort((a, b) => b.likes - a.likes)
+              .map((recipe, index) => <RecipeCard key={index} recipe={recipe} />)
+              .slice(0, 3)}
+          </div>
         </div>
-        <h1 className="text-2xl font-bold mb-4">Uusimmat reseptit</h1>
-        <div className="flex flex-wrap justify-around">
-          {recipes
-            .sort((a, b) => new Date(b._createdAt) - new Date(a._createdAt))
-            .map((recipe, index) => (
-              <RecipeCard key={index} recipe={recipe} />
-            ))}
+        <div className={"max-w-7xl mx-auto"}>
+          <h1 className="text-2xl font-bold mb-4">Uusimmat reseptit</h1>
+          <div className="flex flex-wrap justify-around">
+            {recipes
+              .sort((a, b) => new Date(b._createdAt) - new Date(a._createdAt))
+              .map((recipe, index) => <RecipeCard key={index} recipe={recipe} />)
+              .slice(0, 3)}
+          </div>
         </div>
       </div>
     </Layout>
