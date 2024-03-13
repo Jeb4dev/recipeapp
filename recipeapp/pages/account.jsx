d22268037b6f040ffb3c42d1aff766757bad753e
RemesTop <topsugg02@gmail.com>
2024-03-13 12:59:08 +0200

    Omat reseptit account pageen

--- recipeapp/pages/account.jsx
+++ recipeapp/pages/account.jsx
@@ -3,7 +3,10 @@ import { request } from '../lib/datocms';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
+import RecipeCard from '../components/RecipeCard';
+
 const AccountPage = (props) => {
+
   const { data } = props;
   const user = data.user;
   const router = useRouter();
@@ -46,6 +49,15 @@ const AccountPage = (props) => {
       console.error('Failed to edit user data');
     }
   };
+
+  const recipes = props.data.ownRecipes;
+
+  // Tarkista, että recipes on määritelty ja se on taulukko
+  if (!recipes || !Array.isArray(recipes)) {
+    console.error('recipes ei ole taulukko tai se ei ole määritelty');
+    // Reseptejä ei löytynyt WIP
+    return <div>Reseptejä ei löytynyt.</div>;
+  }
 
   return (
     <Layout>
@@ -101,11 +113,25 @@ const AccountPage = (props) => {
           Logout
         </button>
       </div>
+
+      <div className={'bg-red-50'}>
+        <div className={'max-w-7xl mx-auto'}>
+          <h1 className="text-2xl font-bold my-4">Omat reseptit</h1>
+          <div className="flex flex-wrap justify-between">
+            {recipes
+              .sort((a, b) => new Date(b._createdAt) - new Date(a._createdAt))
+              .map((recipe, index) => <RecipeCard key={index} recipe={recipe} />)
+              }
+          </div>
+        </div>
+        
+      </div>
     </Layout>
   );
 };
 
 export default AccountPage;
+
 
 const ACCOUNT_QUERY = `
   query UserById($id: ItemId) {
@@ -117,40 +143,77 @@ const ACCOUNT_QUERY = `
     }
   }
 `;
+const OWN_RECIPES_QUERY = `
+  query RecipesByUser($Id: ItemId) {
+    allRecipes(filter: {id: {eq: $Id}}) {
+      id
+      title
+      likes
+      description
+      _createdAt
+      image {
+        responsiveImage {
+          alt
+          aspectRatio
+          base64
+          bgColor
+          height
+          sizes
+          src
+          srcSet
+          title
+          webpSrcSet
+          width
+        }
+      }
+    }
+  }
+`;
 
 export async function getServerSideProps(context) {
   const { user } = context.query;
   const cookies = context.req.headers.cookie;
   let data;
+  let ownRecipesData;
+  let session;
 
-  if (user) {
-    data = await request({
-      query: ACCOUNT_QUERY,
-      variables: { id: user },
-    });
-  } else {
-    if (!cookies) {
-      return {
-        redirect: {
-          destination: '/login',
-          permanent: false,
-        },
-      };
-    }
-    const cookieString = decodeURIComponent(cookies);
-    const session = JSON.parse(
+  if (!cookies) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  const cookieString = decodeURIComponent(cookies);
+    session = JSON.parse(
       cookieString
         .split(';')
         .find((c) => c.trim().startsWith('session'))
         .split('=')[1],
     );
-    data = await request({
-      query: ACCOUNT_QUERY,
-      variables: { id: session.userId },
-    });
-  }
 
+    if (user) {
+      data = await request({
+        query: ACCOUNT_QUERY,
+        variables: { id: user },
+      });
+    } else {
+      data = await request({
+        query: ACCOUNT_QUERY,
+        variables: { id: session.userId },
+      });
+    }
+
+  // Hakee käyttäjän omat reseptit käyttäen OWN_RECIPES_QUERY-kyselyä
+  ownRecipesData = await request({
+    query: OWN_RECIPES_QUERY,
+    variables: { userId: session.userId },
+  });
+
+  // Account data ja ownRecipesData
   return {
-    props: { data },
+    props: { data: { ...data, ownRecipes: ownRecipesData.allRecipes } },
   };
 }
