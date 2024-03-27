9203923ec2c6574964bd0a6ad68fbd429bd214d7
Jeb <commit@jeb4.dev>
2024-03-27 11:21:59 +0200

    do not show other user info

--- recipeapp/pages/account.jsx
+++ recipeapp/pages/account.jsx
@@ -1,8 +1,8 @@
-import Layout from '../components/layout';
-import { request } from '../lib/datocms';
-import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { request } from '../lib/datocms';
 
+import Layout from '../components/layout';
 import RecipeCard from '../components/RecipeCard';
 
 const AccountPage = (props) => {
@@ -56,68 +56,75 @@ const AccountPage = (props) => {
   if (!recipes || !Array.isArray(recipes)) {
     console.error('recipes ei ole taulukko tai se ei ole määritelty');
     // Reseptejä ei löytynyt WIP
-    return <div>No recipes found</div>;
+    return <div>Reseptejä ei löytynyt</div>;
   }
 
   return (
     <Layout>
-      <title>Account</title>
+      <title>{username ? `${username} Profiili` : 'Loading...'}</title>
       <div className="p-4 mx-auto max-w-3xl">
-        <h1 className="text-2xl font-bold mb-4">Account Page</h1>
-        <form onSubmit={handleEdit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold mb-4">{username}</h1>
+        {email.length > 0 && (
+          <div>
+            <form onSubmit={handleEdit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                  Käyttäjänimi
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                  Sähköpostiosoite
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                  Salasana
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Päivitä
+                </button>
+              </div>
+            </form>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
+              onClick={handleLogout}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
             >
-              Update
+              Kirjaudu ulos
             </button>
           </div>
-        </form>
-        <button onClick={handleLogout} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          Logout
-        </button>
+        )}
       </div>
 
       <div className="bg-red-50">
         <div className="max-w-7xl mx-auto">
-          <h1 className="text-2xl font-bold my-4">Own recipes</h1>
+          <h1 className="text-2xl font-bold my-4">{username}:n reseptit</h1>
           <div className="flex flex-wrap justify-start gap-8">
             {userRecipes.length > 0 ? (
               userRecipes
@@ -131,20 +138,22 @@ const AccountPage = (props) => {
           </div>
         </div>
 
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-2xl font-bold my-4">Favorites</h1>
-          <div className="flex flex-wrap justify-start gap-8">
-            {user.favorites.length > 0 ? (
-              user.favorites
-                .sort((a, b) => new Date(b._createdAt) - new Date(a._createdAt))
-                .map((recipe, index) => <RecipeCard key={index} recipe={recipe} />)
-            ) : (
-              <div style={{ marginTop: '30px', marginBottom: '50px' }}>
-                <p>No favorites</p>
-              </div>
-            )}
+        {user.email.length > 0 && (
+          <div className="max-w-7xl mx-auto">
+            <h1 className="text-2xl font-bold my-4">Tallennetut reseptit</h1>
+            <div className="flex flex-wrap justify-start gap-8">
+              {user.favorites.length > 0 ? (
+                user.favorites
+                  .sort((a, b) => new Date(b._createdAt) - new Date(a._createdAt))
+                  .map((recipe, index) => <RecipeCard key={index} recipe={recipe} />)
+              ) : (
+                <div style={{ marginTop: '30px', marginBottom: '50px' }}>
+                  <p>No favorites</p>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </Layout>
   );
@@ -248,6 +257,11 @@ export async function getServerSideProps(context) {
       query: ACCOUNT_QUERY,
       variables: { id: session.userId },
     });
+  }
+
+  if (user && user !== session.userId) {
+    data.user.password = '';
+    data.user.email = '';
   }
 
   // Hakee käyttäjän omat reseptit käyttäen OWN_RECIPES_QUERY-kyselyä
